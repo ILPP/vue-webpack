79b9804e1f8fba50dc632e3b9d5c35754635fa19
libingxin <1974317107@qq.com>
2022-02-13 13:02:14 +0800

    chore

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import App from './App.vue';
 import Vue from 'vue';
 import { Table, TableColumn } from 'element-ui';
+console.log(a);
+const a = 1;
 
 Vue.use(Table);
 Vue.use(TableColumn);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
